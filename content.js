7b7288f22c12e7c5cf0128a96402d72e51cac5d9
purgatorian <memedkipik@gmail.com>
2022-10-22 12:26:46 +0300

    first commit

---
+++ content.js
@@ -0,0 +1,62 @@
+function createButtons() {
+    const btnplacement = document.getElementsByClassName("u__cf inbox__conversation-controls__info-area js__conversation-controls__info-area")[0];
+    if (btnplacement) {
+        userButton = document.createElement("button");
+        userButton.innerHTML = "  Userid  ";
+        userButton.id = "UserButton";
+        userButton.type = "button";
+        userButton.class="btn o__primary ember-view"
+        userButton.style.border = " 7px solid #334bfa"
+        userButton.style.color = "#ffffff"
+        userButton.style.backgroundColor = "#334bfa"
+        userButton.style.margin = "5px"
+        userButton.style.borderRadius = "5px"
+
+        jiraButton = document.createElement("button");
+        jiraButton.innerHTML = "  Jira  ";
+        jiraButton.id = "jiraButton";
+        jiraButton.type = "button";
+        jiraButton.class="btn o__primary ember-view"
+        jiraButton.style.border = " 7px solid #334bfa"
+        jiraButton.style.color = "#ffffff"
+        jiraButton.style.backgroundColor = "#334bfa"
+        jiraButton.style.margin = "5px"
+        jiraButton.style.borderRadius = "5px"
+        btnplacement.appendChild(userButton)
+        btnplacement.appendChild(jiraButton)
+    } else {
+        setTimeout(createButtons, 300); // try again in 300 milliseconds
+    }
+}
+function pollDOM (jiraButton, userButton) {
+    const userid = document.getElementsByClassName("conversation__card__title__em-link no-underline hover:underline");
+    if (userid.length) {
+        grabbedID = userid[0].innerText;
+        cleanUserID = grabbedID.split("(")
+        if (cleanUserID[1].includes(")")){
+            finalUserID = cleanUserID[1].replace(")",'')
+        } else {
+            finalUserID = cleanUserID[1]
+        };
+        jiraButton = document.getElementById("jiraButton")
+        userButton = document.getElementById("UserButton")
+        jiraButton.addEventListener("click",function(){
+            pollDOM()
+            currentUrl = window.location.href
+            textForJira = "Ticket ID : " + currentUrl + "\n" + "UserID: " + finalUserID
+            navigator.clipboard.writeText(textForJira)
+        })
+        userButton.addEventListener("click",function(){
+        pollDOM()
+        navigator.clipboard.writeText(finalUserID)
+        })
+    } else {
+        setTimeout(pollDOM, 300); // try again in 300 milliseconds
+    };
+}
+createButtons()
+pollDOM()
+
+
+
+   
